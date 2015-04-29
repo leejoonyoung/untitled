28a47a092141a6bb722089cb1d8c2dd9e12b16c0
unknown <jylee3@jylee3.humaxdigital.com>
2015-04-29 17:27:51 +0900

    Initial commit

---
+++ Test.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'jylee3'
+
+tuple = (4, 5)
+array = [1, 2]
+map = {
+    'name': "조성우"
+    '직책' : "차장"
+}
+x, y = truple
+
+print "start out"
+print p
+print x
+print y
